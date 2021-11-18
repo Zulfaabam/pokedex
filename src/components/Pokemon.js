6f62152756b1394b14fe5f0c1da557231224188b
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-11-18 14:24:19 +0700

    added pokemon data

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router'
+import './Pokemon.css'
 
 export default function Pokemon() {
   const [data, setData] = useState([])
@@ -20,8 +21,11 @@ export default function Pokemon() {
   console.log(data)
 
   return (
-    <div className="pkmn-details">
-      <div>{data.name}</div>
+    <div className="pkmn-details grid-container">
+      <h1>{data.name}</h1>
+      <img src={data.sprites.front_default} alt={data.name}></img>
+      <h2>Pokedex Data</h2>
+      <h2>Base Stats</h2>
     </div>
   )
 }
