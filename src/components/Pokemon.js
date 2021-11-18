f288f104457d4dcf5c531c209889a053b30cc348
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-11-18 15:19:02 +0700

    added pokemon data

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -11,21 +11,45 @@ export default function Pokemon() {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
       .then((res) => {
-        const { data } = res
-        setData(data)
+        // const { data } = res
+        setData(res.data)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [pokemonId])
+  console.log(pokemonId)
   console.log(data)
 
   return (
-    <div className="pkmn-details grid-container">
+    <div className="pokemon grid-container">
       <h1>{data.name}</h1>
-      <img src={data.sprites.front_default} alt={data.name}></img>
-      <h2>Pokedex Data</h2>
-      <h2>Base Stats</h2>
+      {/* <img src={data.sprites.front_default} alt={data.name}></img> */}
+      <div className="pkmn-data-wrapper">
+        <h2>Pokedex Data</h2>
+        {data.types.map((item, index) => (
+          <p key={index}>{item.type.name}</p>
+        ))}
+        <p>Height: {data.height}</p>
+        <p>Weight: {data.weight}</p>
+        {data.abilities.map((item, index) => (
+          <p key={index}>
+            {item.is_hidden
+              ? `${item.ability.name} (Hidden)`
+              : item.ability.name}
+          </p>
+        ))}
+        <h2>Base Stats</h2>
+        {data.stats.map((item, index) => (
+          <p key={index}>
+            {item.stat.name}: {item.base_stat}
+          </p>
+        ))}
+        <h2>Moves</h2>
+        {data.moves.map((item, index) => (
+          <p key={index}>{item.move.name}</p>
+        ))}
+      </div>
     </div>
   )
 }
