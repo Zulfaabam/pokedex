9b0be145a1973ee891e6bc5d835f1de23ba64372
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-11-23 10:19:38 +0700

    added github api to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.css'
+import axios from 'axios'
 
 export default function About() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // setLoading(true)
+      try {
+        const response = await axios.get(
+          `https://api.github.com/users/Zulfaabam`
+        )
+        if (response.status === 200) {
+          setData(response.data)
+          // setLoading(false)
+        }
+      } catch (err) {
+        // setLoading(true)
+        // setError(err)
+      }
+    }
+
+    fetchData()
+  }, [])
+  console.log(data)
+
   return (
     <div className="about grid-container">
       <h1>About App</h1>
@@ -15,14 +39,19 @@ export default function About() {
         to get the Pokemon data.
       </p>
       <h2>About Author</h2>
-      <img
-        src="https://avatars.githubusercontent.com/u/63113235?v=4"
-        alt="author pic"
-      />
+      <img src={data.avatar_url} alt="author pic" />
       <p>
-        My name is Zulfa Fatah Akbar Ahmad from group 34 PMDP. I am a Computer
-        Engineering student at Diponegoro University. Passionate on Front-end
-        Developer.
+        My name is {data.name} from group 34 PMDP. I am a Computer Engineering
+        student at Diponegoro University. Passionate on Front-end Developer. Go
+        check My{' '}
+        <a href={data.html_url} target="_blank" rel="noreferrer">
+          Github
+        </a>{' '}
+        and My{' '}
+        <a href={data.blog} target="_blank" rel="noreferrer">
+          Website
+        </a>{' '}
+        for another project.
       </p>
     </div>
   )
